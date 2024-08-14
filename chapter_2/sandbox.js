7a1c4b1a33ae902616e4a3465e0cf6be24579945
zentrois <zentrois@gmail.com>
2024-08-15 03:58:23 +0800

    7. Variables, Constants & Comments

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,3 +1,17 @@
-console.log(1);
-console.log(2);
+let age = 25;
+let year = 2019;
 
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+
+console.log(points);
+
+// points = 50; // This will throw an error
+
+var score = 75;
+
+console.log(score);
